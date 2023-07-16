fb4104cbe960b30aa5d643b6690f605fd6d33dca
BovineBeta <bovinefbik@foxmail.com>
2023-07-16 09:57:36 +0800

    fix: 完善"欢迎使用", 更改js布局以及完善注释

--- js/main.js
+++ js/main.js
@@ -2,16 +2,34 @@ console.log('made by FBIK.');
 
 let $ = mdui.$;
 
+/*
+ * 数据处理函数声明 起
+ */
+
+/**
+ * @description: 随机生成一定范围的整数 
+ * @param {Number} min=0 - 最小值, 整数
+ * @param {Number} max - 最大值, 整数
+ * @return {Number} - 随机生成的整数
+ */
+const randomInt = (min = 0, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+/*
+ * 数据处理函数声明 终
+ */
+
+/*
+ * 核心功能 起
+ */
+
 // 页面内容配置
 let page = {
     name: ['index', 'welcome'],
     changer: new mdui.Tab('#page-changer'),
-    lab: new mdui.Fab('#fab-wrapper')
-}
-
-// 随机整数生成器
-const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-
+    lab: new mdui.Fab('#fab-wrapper'),
+    fn: {}
+};
+console.log(page.lab);
 
 // 右上角mdui menu主题色更换按钮
 let darkModeState = window.matchMedia('(prefers-color-scheme:dark)').matches;
@@ -29,6 +47,37 @@ $('#theme-changer').on('click', () => {
     darkModeState = !darkModeState;
 });
 
+// 使用js调用mdui tab选项卡, 实现页面切换
+page.name.forEach((element, index) => {
+    $('#link-' + element).on('click', () => page.changer.show(index));
+});
+
+//* 悬浮按钮 mdui-fab 逻辑 起
+$('#page-changer').on('change.mdui.tab', event => {
+    switch (event._detail.index) {
+        case 1:
+            page.lab.show();
+            break;
+        default:
+            page.lab.hide();
+    };
+});
+
+
+//* 悬浮按钮 mdui-fab 逻辑 终
+
+/*
+ * 核心功能 终
+ */
+
+/* 
+ * 页面功能声明区 起
+ */
+
+// 临时用于切换到默认页面
+page.changer.show(1)
+
+//* 首页 index
 
 // 一言
 fetch('./js/json/yiyan.json')
@@ -38,19 +87,13 @@ fetch('./js/json/yiyan.json')
         console.error('一言读取出现错误:', error);
     });
 
+//* 欢迎使用 welcome
 
-/* 使用js调用mdui tab选项卡, 实现页面切换 */
-page.name.forEach((element, index) => {
-    $('#link-' + element).on('click', () => page.changer.show(index));
+new mdui.Tooltip('#tooltip-番茄工作法', {
+    content: '\"番茄工作法\"是由 弗朗西斯科·西里洛 于1992年创立的一种相对于GTD(Getting Things Done)更微观的时间管理方法'
 });
 
-// 悬浮按钮 lab-wapper 逻辑
-$('#page-changer').on('change.mdui.tab', event => {
-    switch (event._detail.index) {
-        case 1:
-            page.lab.show();
-            break;
-        default:
-            page.lab.hide();
-    };
-})
+
+/* 
+ * 页面功能声明区 终
+ */
