3f5e14ac5b1373d224854c6c3016827832ef95de
BovineBeta <bovinefbik@foxmail.com>
2023-07-25 22:05:12 +0800

    feat: 番茄钟(7/8) delay 没有按照预想的方式工作但是跑起来了(

--- src/index.js
+++ src/index.js
@@ -30,31 +30,6 @@ if (window.location.host === 'self-study-kit.vercel.app') {
  * @return {Number} 随机生成的整数
  */
 const randomInt = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
-/**
- * @description 根据一个时间戳和样式生成一个时间文本
- * @param {string} [style='{年}/{月}/{日} {时}:{分}:{秒}'] 时间样式字符串, 默认为'{年}/{月}/{日} {时}:{分}:{秒}'
- * @param {Date} [time=new Date()] 一个时间戳
- * @return {string} 生成的时间文本
- */
-function timeTextDiy(style = '{年}/{月}/{日} {时}:{分}:{秒}', time = new Date()) {
-    let god = new Date(time);
-    let DIZHI = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
-    let HANZI = ['天', '一', '二', '三', '四', '五', '六'];
-    let type = {
-        年: god.getFullYear(),
-        月: god.getMonth() + 1,
-        日: god.getDate(),
-        时: god.getHours().toString().padStart(2, '0'),
-        分: god.getMinutes().toString().padStart(2, '0'),
-        秒: god.getSeconds().toString().padStart(2, '0'),
-        毫秒: god.getMilliseconds(),
-        星期: HANZI[god.getDay()],
-        时辰: `${DIZHI[[Math.floor((god.getHours() % 12) + god.getMinutes() / 60)]]}时${
-            HANZI[Math.floor(god.getMinutes() / 15)]
-        }刻`,
-    };
-    return style.replace(/{([^}]+)}/g, (match, p1) => type[p1]);
-}
 
 /*
  * 数据处理函数声明 终
@@ -118,19 +93,44 @@ let page = {
         },
         clock: {
             /**
+             * @description 根据一个时间戳和样式生成一个时间文本
+             * @param {string} [style='{年}/{月}/{日} {时}:{分}:{秒}'] 时间样式字符串, 默认为'{年}/{月}/{日} {时}:{分}:{秒}'
+             * @param {Date} [time=new Date()] 一个时间戳
+             * @return {string} 生成的时间文本
+             */
+            timeTextDiy: function (style = '{年}/{月}/{日} {时}:{分}:{秒}', time = new Date()) {
+                let god = new Date(time);
+                let DIZHI = ['子', '丑', '寅', '卯', '辰', '巳', '午', '未', '申', '酉', '戌', '亥'];
+                let HANZI = ['天', '一', '二', '三', '四', '五', '六'];
+                let type = {
+                    年: god.getFullYear(),
+                    月: god.getMonth() + 1,
+                    日: god.getDate(),
+                    时: god.getHours().toString().padStart(2, '0'),
+                    分: god.getMinutes().toString().padStart(2, '0'),
+                    秒: god.getSeconds().toString().padStart(2, '0'),
+                    毫秒: god.getMilliseconds(),
+                    星期: HANZI[god.getDay()],
+                    时辰: `${DIZHI[[Math.floor((god.getHours() % 12) + god.getMinutes() / 60)]]}时${
+                        HANZI[Math.floor(god.getMinutes() / 15)]
+                    }刻`,
+                };
+                return style.replace(/{([^}]+)}/g, (match, p1) => type[p1]);
+            },
+            /**
              * @description 创建clock实例并启动
-             * @param {string} [target='#page-clock-text'] 目标, 使用css选择器, 默认为#page-clock-text
+             * @param {string} [target='#page-clock-text'] 目标DOM, 使用css选择器, 默认为#page-clock-text
              * @param {string} style 时间样式字符串, 默认为'{年}/{月}/{日} {时}:{分}:{秒}'
              * @param {number} [delay=100] 间隔时间, 默认为100
              */
             start: function (target = '#page-clock-text', style, delay = 100) {
                 this.instances[target] = setInterval(() => {
-                    $(target).text(timeTextDiy(style));
+                    $(target).text(this.timeTextDiy(style));
                 }, delay);
             },
             /**
              * @description 停止并删除clock实例
-             * @param {string} [target='#page-clock-text'] 目标, 使用css选择器, 默认为#page-clock-text
+             * @param {string} [target='#page-clock-text'] 目标DOM, 使用css选择器, 默认为#page-clock-text
              */
             stop: function (target = '#page-clock-text') {
                 clearInterval(this.instances[target]);
@@ -138,10 +138,90 @@ let page = {
             },
             instances: {},
         },
-        'pomodoro-timer': {
-            run: false,
-            start: function () {},
-            stop: function () {},
+        pomodoro_timer: {
+            /**
+             * @description 计算目标时间与现在时间之差
+             * @param {*} time 目标时间戳
+             * @return {string | void} 时间未过则返回'剩余分钟:剩余时间', 时间已过则无返回
+             */
+            getTimeDiff: function (time) {
+                let diff = new Date(time).getTime() - new Date().getTime();
+                console.log('diff', diff);
+                return diff < 0
+                    ? void 0
+                    : Math.floor((diff / (1000 * 60)) % 60)
+                          .toString()
+                          .padStart(2, '0') +
+                          ':' +
+                          Math.floor((diff / 1000) % 60)
+                              .toString()
+                              .padStart(2, '0');
+            },
+            /**
+             * @description 创建并启动番茄钟实例
+             * @param {string} [targetDOM='#page-pomodoro-timer-display div'] 目标DOM, 使用css选择器, 默认为'#page-pomodoro-timer-display div'
+             * @param {number} [delay=1000] 间隔时间, 默认为1000
+             * @param {Number} pomodoro_time 专注时间
+             * @param {Number} short_break_time 休息时间
+             * @param {Number} [long_break_time=undefined] 长休息时间
+             */
+            init: function (
+                targetDOM = '#page-pomodoro-timer-display div',
+                delay = 1000,
+                pomodoro_time,
+                short_break_time,
+                long_break_time,
+            ) {
+                this.instances[targetDOM] = {
+                    status: 0,
+                    settings: {
+                        pomodoro: pomodoro_time,
+                        short_break: short_break_time,
+                        long_break: long_break_time,
+                    },
+                };
+                $('#button-menu').on('click', () => {
+                    page.drawer.dom.close();
+                });
+                this.next(targetDOM, this.instances[targetDOM].settings.pomodoro, delay);
+            },
+            /**
+             * @description 切换番茄钟实例到下个阶段. 如果刚刚初始化, 则是启动番茄钟实例
+             * @param {string} targetDOM 目标DOM, 使用css选择器
+             * @param {number} delay 间隔时间
+             */
+            next: function (targetDOM, delay) {
+                // 浅拷贝
+                let instance = this.instances[targetDOM];
+                delete instance.interval;
+
+                let status = instance.status;
+                let settings = instance.settings;
+                (status === 2 && settings.long_break === void 0) || status === 3 ? (status = 1) : ++status;
+                let nextStatus = (status === 2 && settings.long_break === void 0) || status === 3 ? 1 : status + 1;
+                let phase = [void 0, 'pomodoro', 'short_break', 'long_break'];
+                let phaseName = ['初始化', '专注时间', '休息时间', '长休息时间'];
+                $(targetDOM).eq(0).text(phaseName[status]);
+                let time = new Date().getTime() + settings[phase[status]] * 1000 * 60;
+                $(targetDOM)
+                    .eq(2)
+                    .text('下一阶段: ' + phaseName[nextStatus]);
+                this.instances[targetDOM].interval = setInterval(() => {
+                    $(targetDOM).eq(1).text(this.getTimeDiff(time));
+                }, delay);
+            },
+            /**
+             * @description 停止番茄钟实例
+             * @param {string} [target='#page-pomodoro-timer-display div'] 目标DOM, 使用css选择器, 默认为'#page-pomodoro-timer-display div'
+             */
+            stop: function (target = '#page-pomodoro-timer-display div') {
+                clearInterval(this.instances[target].interval);
+                delete this.instances[target];
+                $('#button-menu').on('click', () => {
+                    page.drawer.dom.open();
+                });
+            },
+            instances: {},
         },
     },
 };
@@ -257,17 +337,14 @@ $('#page-pomodoro-timer-options input')
             .text(checked ? '长休息时间' : '已关闭 长休息时间');
         $('#page-pomodoro-timer-options .mdui-textfield').eq(2).removeClass('mdui-textfield-invalid');
     });
-page.pomodoro_timer.changer.show(0);
 $('#button-pomodoro-timer-start').on('click', () => {
-    console.log($('#page-pomodoro-timer-options input').eq(3).prop('checked') ? '开' : '关');
-
     let hasError = false;
     [0, 1, 2].forEach(element => {
         let warning = $('#page-pomodoro-timer-options .mdui-textfield-error').eq(element);
         let input = $('#page-pomodoro-timer-options input').eq(element);
         let textfield = $('#page-pomodoro-timer-options .mdui-textfield').eq(element);
         if (
-            /^[1-9]\d*$/.test(input.val()) ||
+            /^[1-9]|[1-5]\d$/.test(input.val()) ||
             (element === 2 && !$('#page-pomodoro-timer-options input').eq(3).prop('checked'))
         ) {
             warning.text('');
@@ -275,15 +352,11 @@ $('#button-pomodoro-timer-start').on('click', () => {
         } else {
             warning.text('内容必须为正整数');
             hasError = true;
-            console.log(warning);
             textfield.addClass('mdui-textfield-invalid');
         }
     });
 
     if (!hasError) {
-        $('#button-menu').on('click', () => {
-            page.drawer.dom.close();
-        });
         let input = $('#page-pomodoro-timer-options input');
         let settings = {
             pomodoro: Number(input.eq(0).val()),
@@ -291,9 +364,20 @@ $('#button-pomodoro-timer-start').on('click', () => {
             long_break: Number(input.eq(2).val()),
         };
         page.pomodoro_timer.changer.show(1);
+        // 倒计时
+        page.fn.pomodoro_timer.init(
+            '#page-pomodoro-timer-display div',
+            1000,
+            settings.pomodoro,
+            settings.short_break,
+            settings.long_break,
+        );
     }
 });
-
+$('#button-pomodoro-timer-next').on('click', () => {
+    page.fn.pomodoro_timer.next('#page-pomodoro-timer-display div', 1000);
+});
+$('#button-pomodoro-timer-stop').on('click', () => {});
 /*
  * 页面功能声明区 终
  */
