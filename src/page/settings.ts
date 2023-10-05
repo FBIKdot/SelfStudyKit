690f015e05541eb98cc4d52a9fea3638aa974931
BovineBeta <bovinefbik@foxmail.com>
2023-10-05 11:42:58 +0800

    refactor(settings): [WIP]模块化编程

--- src/page/settings.ts
+++ src/page/settings.ts
@@ -1,12 +1,8 @@
 import mdui from 'mdui';
 import { getCookie } from 'typescript-cookie';
-import { themeChanger } from './main';
+import { themeChanger } from './function/main';
 
 let $ = mdui.$;
-
-// Cookie
-const themeStatus: string = getCookie('theme') || 'auto';
-themeChanger(themeStatus);
 
 // 锁住panel. 因为懒得设计ui
 $('#page-settings-panel .mdui-panel-item')
@@ -15,12 +11,18 @@ $('#page-settings-panel .mdui-panel-item')
         $(element).on('click', () => {});
     });
 
+// 尝试从cookit获取主题信息
+const themeStatus: 'auto' | 'light' | 'dark' = ['auto', 'light', 'dark'].includes(String(getCookie('theme')))
+    ? (getCookie('theme') as 'auto' | 'light' | 'dark')
+    : 'auto';
+themeChanger(themeStatus);
+
 // 勾选当前主题色
 $(`input[name="主题色"][value="${themeStatus}"]`).prop('checked', true);
 
 // 应用主题色
 $('#page-settings-theme-apply').on('click', () => {
-    const themeStatus = $('input[name="主题色"]:checked').val() as string;
+    const themeStatus = $('input[name="主题色"]:checked').val() as 'auto' | 'light' | 'dark';
     themeChanger(themeStatus);
     $(`input[name="主题色"][value="${themeStatus}"]`).prop('checked', true);
 });
