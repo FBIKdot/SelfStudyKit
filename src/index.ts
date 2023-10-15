b24e191966efdd28727a16f0a418ec32b02205af
BovineBeta <bovinefbik@foxmail.com>
2023-10-15 14:23:18 +0800

    refactor(clock&init): 重构Clock与内容生成. 未经过调试

--- src/index.ts
+++ src/index.ts
@@ -10,82 +10,74 @@ import './css/main.css';
 import page from './page';
 import { getCookie, setCookie } from 'typescript-cookie';
 
+import config from './page/function/config';
+import { pageChanger, themeChanger } from './page/function/main';
+import { Clock } from './page/function/clock';
+
 let $ = mdui.$;
 
 // 等css加载完再显示 body
 $('body').removeAttr('style');
 
-/*
- * 核心功能 起
- */
-// 绑定左上角button按钮打开drawer. 更改属性无法禁用drawer
-$('#button-menu').on('click', () => page.drawer.dom.toggle());
+namespace Init {
+    const drawer = new mdui.Drawer('#drawer');
 
-//* 生成drawer侧边栏. 使用js调用mdui tab选项卡, 实现页面切换
-page.name.forEach((element: string, index: number) => {
-    $('#drawer-list').append(
-        `${
-            page.drawer.subheader[element]
-                ? '<div class="mdui-subheader">' + page.drawer.subheader[element] + '</div>'
-                : ''
-        } ` +
-            `<a class="mdui-list-item mdui-ripple" id="link-${element}">` +
-            `<i class="mdui-list-item-icon mdui-icon material-icons">${page.config[element].icon}</i>` +
-            `<div class="mdui-list-item-content">${page.config[element].title}</div></a> `,
-    );
-    $('#link-' + element).on('click', () => {
-        // 切换页面
-        page.changer.show(index);
+    // 绑定左上角button按钮打开drawer. 更改属性无法禁用drawer
+    $('#button-menu').on('click', () => page.drawer.dom.toggle());
 
-        // 根据屏幕宽度判断drawer是否启用遮罩, 如果是则在点击后关闭drawer
-        if ($(window).width() < 1024) {
-            page.drawer.dom.close();
-        }
+    //* 生成drawer侧边栏. 使用js调用mdui tab选项卡, 实现页面切换
+    config.name.forEach((element: string, index: number) => {
+        const { icon, title } = config.config[element as keyof typeof config.config];
+        const subheader: string = config.drawer.subheader[element as keyof typeof config.drawer.subheader] || '';
+        $('#drawer-list').append(
+            `${subheader ? `<div class="mdui-subheader">${subheader}</div>` : ''} ` +
+                `<a class="mdui-list-item mdui-ripple" id="link-${element}">` +
+                `<i class="mdui-list-item-icon mdui-icon material-icons">${icon}</i>` +
+                `<div class="mdui-list-item-content">${title}</div></a> `,
+        );
+
+        $('#link-' + element).on('click', () => {
+            // 切换页面
+            pageChanger(index);
+
+            // 根据屏幕宽度判断drawer是否启用遮罩, 如果是则在点击后关闭drawer
+            if ($(window).width() < 1024) {
+                drawer.close();
+            }
+        });
     });
-});
-
-/*
- * 核心功能 终
- */
-
-/*
- * 页面功能声明区 起
- */
-//* 页面切换 逻辑
-// 切换页面
-const pageNumber: number = Number(getCookie('page') || 0);
-page.changer.show(pageNumber);
-
-// 切换标题
-$('.mdui-typo-title').text(page.config[page.name[pageNumber]].title);
-
-// 是否启用clock
-if (getCookie('page') === '2') {
-    page.fn.clock.start('#page-clock-text', '{年}/{月}/{日} {时}:{分}:{秒} {时辰}');
 }
 
-// 页面切换事件监听
-$('#page-changer').on('change.mdui.tab', (event: Event) => {
-    const pageNumber: number = (event as any)._detail.index;
+//* 页面切换 逻辑
+// 切换页面
 
-    //默认隐藏fab
-    page.fab.hide();
-    page.fn.clock.stop();
-    // 根据mdui文档, 这里的event._detail绝对存在
-    switch ((event as any)._detail.index) {
-        case 1:
-            // page.fab.show();
-            break;
-        case 2:
-            page.fn.clock.start('#page-clock-text', '{年}/{月}/{日} {时}:{分}:{秒} {时辰}');
-            break;
-    }
-    // 切换标题
-    $('.mdui-typo-title').text(page.config[page.name[pageNumber]].title);
-    // 存储页面页码
-    setCookie('page', pageNumber.toString());
-    console.log('pageNumber.toString()', pageNumber.toString());
-});
+namespace pageChanging {
+    // 页面切换事件监听
+    $('#page-changer').on('change.mdui.tab', (event: Event) => {
+        const pageNumber: number = (event as any)._detail.index;
+
+        //默认隐藏fab
+        page.fab.hide();
+        page.fn.clock.stop();
+        // 根据mdui文档, 这里的event._detail绝对存在
+        switch (pageNumber) {
+            case 1:
+                // page.fab.show();
+                break;
+            case 2:
+                Clock.start();
+                break;
+        }
+        // 切换标题
+        $('.mdui-typo-title').text(page.config[page.name[pageNumber]].title);
+        // 存储页面页码
+        setCookie('page', pageNumber.toString());
+        console.log('pageNumber.toString()', pageNumber.toString());
+    });
+
+    const pageNumber: number = Number(getCookie('page') || 0);
+    pageChanger(pageNumber);
+}
 
 //* 测试区 起
 // 测试用功能:
@@ -129,6 +121,3 @@ import './page/settings';
 
 // 版本检查
 // page.fn.version_Check();
-/*
- * 页面功能声明区 终
- */
