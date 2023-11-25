85cbb515cf6309dee66a1a01ad0729d2c73eaf23
BovineBeta <bovinefbik@foxmail.com>
2023-11-25 17:39:29 +0800

    chore: 在生产模式下忽略console和debugger

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,24 @@
 import { defineConfig } from 'vite';
 import { createHtmlPlugin } from 'vite-plugin-html';
 
-export default defineConfig({
-    server: {
-        port: 5500,
-        strictPort: true,
-    },
-    build: {
-        target: 'es2015',
-    },
-    plugins: [
-        createHtmlPlugin({
-            minify: true,
-            entry: 'src/index.ts',
-            template: 'index.html',
-        }),
-    ],
+export default defineConfig(({ command, mode }) => {
+    return {
+        server: {
+            port: 5500,
+            strictPort: true,
+        },
+        build: {
+            target: 'es2015',
+        },
+        esbuild: {
+            drop: mode === 'production' ? ['console', 'debugger'] : [],
+        },
+        plugins: [
+            createHtmlPlugin({
+                minify: true,
+                entry: 'src/index.ts',
+                template: 'index.html',
+            }),
+        ],
+    };
 });
